e70979acd8ae46dc5742df34cd35db1e2377c60b
Ivan Jovkovic <jokac14@gmail.com>
2018-12-17 15:09:02 +0100

    upload css, scss, js, img files

---
+++ public/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+	var height = $("footer").outerHeight();
+	$("#app").css("margin-bottom", "-" + height + "px");
+	$(".push-footer").css("height", height + "px");
+});
+
+$(document).ready(function () {
+	if ($("#back-to-top").length) {
+		var scrollTrigger = 500, // px
+			backToTop = function () {
+				var scrollTop = $(window).scrollTop();
+				if (scrollTop > scrollTrigger) {
+					$("#back-to-top").addClass("show");
+				} else {
+					$("#back-to-top").removeClass("show");
+				}
+			};
+		backToTop();
+		$(window).on("scroll", function () {
+			backToTop();
+		});
+		$("#back-to-top").on("click", function (e) {
+			e.preventDefault();
+			$("html, body").animate({
+				scrollTop: 0
+			}, 1000);
+		});
+	}
+});
+
+function wow() {
+	var windowWidth = $(window).width();
+	if (windowWidth >= 768) {
+		$(".wow").each(function () {
+			var wowHeight = $(this).height();
+			$(this).attr("data-wow-offset", wowHeight);
+		});
+	}
+}
+
+$(document).ready(function () {
+	wow();
+	$(window).resize(function () {
+		wow();
+	});
+});
