7ee9ab48ee3bab59dcdcf4e2896c14eaa5937210
Ivan Jovkovic <jokac14@gmail.com>
2018-12-18 10:13:36 +0100

    navbar

--- resources/views/section/nav.blade.php
+++ resources/views/section/nav.blade.php
@@ -9,20 +9,74 @@
                     <a>Home</a>
                 </li>
                 <li>
-                    <a>About Us</a>
-                </li>
-                <li>
-                    <a>Partner</a>
-                    <ul class="dropdown">
+                    <a class="mphim">Mphim+</a>
+					<ul class="dropdown">
                         <li>
-                            <a>Select ffdfds1</a>
-                            <a>Select 2</a>
-                            <a>Select 3</a>
+                            <a>What is</a>
+						</li>
+						<li>
+                            <a>What does it do</a>
+						</li>
+						<li>
+                            <a>Why to have it</a>
+						</li>
+						<li>
+                            <a>Roadmap</a>
                         </li>
                     </ul>
                 </li>
                 <li>
-                    <a>Team</a>
+                    <a>Customers</a>
+					<ul class="dropdown">
+						<h5 class="text-center">Who can use it</h5>
+                        <li>
+                            <a>Companies</a>
+						</li>
+						<li>
+                            <a>Professionals</a>
+						</li>
+						<li>
+                            <a>Trade associations</a>
+						</li>
+						<li>
+                            <a>Public institutions</a>
+						</li>
+						<li>
+                            <a>Schools</a>
+						</li>
+						<li>
+                            <a>University</a>
+						</li>
+                    </ul>
+                </li>
+                <li>
+                    <a>Versions</a>
+                </li>
+				<li>
+                    <a>Commercial</a>
+                </li>
+                <li>
+                    <a>Reference</a>
+                </li>
+				<li>
+                    <a>Accademy</a>
+					<ul class="dropdown">
+                        <li>
+                            <a>Training4Company</a>
+						</li>
+						<li>
+                            <a>Training4Agent</a>
+						</li>
+						<li>
+                            <a>Training4Advisor</a>
+						</li>
+						<li>
+                            <a>Training4Manager</a>
+						</li>
+                    </ul>
+                </li>
+				<li>
+                    <a>Contact</a>
                 </li>
             </ul>
         </div>
