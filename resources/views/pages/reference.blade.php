3ce87046a53736f999b0e328d07f58636dbb4574
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2019-11-27 14:56:07 +0100

    fix reference images

--- resources/views/pages/reference.blade.php
+++ resources/views/pages/reference.blade.php
@@ -9,12 +9,12 @@
             {{ __('translate.reference') }}
         </h3>
         <div class="row align-items-start">
-            <div class="col-md-4">
+            {{-- <div class="col-md-4">
                 <div class="image">
                     <img src="{{ asset('img/customers/company.jpg')}}" alt="img/customers/company.jpg" />
                 </div>
-            </div>
-            <div class="col-md-8">
+            </div> --}}
+            <div class="col-md-12">
                 {!! $content->{'body_' . $language} !!}
             </div>
         </div>
