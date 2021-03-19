9f3d9f57a5e43f10c6e524110fc0f559cead7115
Uros Djordjevic <31542903+orokusaki91@users.noreply.github.com>
2021-03-19 18:26:14 +0100

    add backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\BackupDatabase',
+        // 'App\Console\Commands\BackupDatabase',
     ];
 
     /**
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('db:backup')->daily();
+        $schedule->command('backup:run --only-db')->everyMinute();
     }
 
     /**
