f7564c0cc33005bb5ad478bd8b9734cf9e064ab7
Uros Djordjevic <31542903+orokusaki91@users.noreply.github.com>
2021-03-19 18:41:38 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:run --only-db')->everyMinute();
+        $schedule->command('backup:run --only-db')->daily();
     }
 
     /**
